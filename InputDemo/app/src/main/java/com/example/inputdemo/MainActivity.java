98ad8c225484b924cfea85dad81a23e4dab546ae
yangweikang <980122114@qq.com>
2021-08-27 19:28:03 +0800

    dpdate:加入Inputdemo

---
+++ InputDemo/app/src/main/java/com/example/inputdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.inputdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 点输入页面
+ * @author yangweikang
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
